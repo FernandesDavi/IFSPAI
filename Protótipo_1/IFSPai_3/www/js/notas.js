5347ce614dd84283bf90fb2457c0b60e0ea620c8
davi <davi.fernandes2012@gmail.com>
2016-08-25 13:15:38 -0300

    Adicionado a função de media falta fazer a media ponderada

--- Protótipo_1/IFSPai_3/www/js/notas.js
+++ Protótipo_1/IFSPai_3/www/js/notas.js
@@ -8,11 +8,14 @@ function carregar(){
     var data = ""; 
 
     var x="";
-   var mat = localStorage.getItem("materia");
-   var aluno = localStorage.getItem("aluno");
+  // var mat = localStorage.getItem("materia");
+   //var aluno = localStorage.getItem("aluno");
   // alert(mat,aluno);
-   // var mat =1;
-    //var aluno =1;
+    var mat =1;
+    var aluno =1;
+var  vl1 = 0;
+var calc;
+var vl3;
 
     //Capturar Dados Usando Método AJAX do jQuery
     $.ajax({
@@ -42,13 +45,22 @@ function carregar(){
 					    itens += "<td colspan='2'><b>" + retorno[i].avaliacao_nome + "</b></td>";
 					    itens += "<td>" + retorno[i].data + "</td>";
 					    itens += "<td>" + retorno[i].calculo + "</td>";
+					    calc = retorno[i].calculo;
 					    itens += "<td><b>" + retorno[i].nota + "</b></td>";
+					    vl1 += parseFloat(retorno[i].nota);
+
 					 	itens += "</tr>";
 					   
 				
 			  }
-			 
+			  if (calc == "MEDIA") {
+				vl3 = (vl1 / i);
+
+			  }
+			 					    
+
 			    //Preencher a Tabela
+			     $("#mediafinal").html(vl3);
 				 $("#dis").html(materia);
 				 $("#turma").html(turma);
 				 $("#prof").html(psor);
